10b4de9a2c44ddc81e37a75fc0f7a1bddea4d2c9
Alex Clark <aclark@aclark.net>
2012-04-16 08:23:51 -0400

    Ship it

--- schema_extender_test/__init__.py
+++
@@ -1,5 +0,0 @@
-from Products.Archetypes.public import BooleanField
-from archetypes.schemaextender.field import ExtensionField
-
-class MyBooleanField(ExtensionField, BooleanField):
-    """A trivial field."""
