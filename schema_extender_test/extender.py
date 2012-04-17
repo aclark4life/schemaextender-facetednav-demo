08af8e8cf370db32d5c504dc30e2ef961bf71c71
Alex Clark <aclark@aclark.net>
2012-04-17 09:02:13 -0400

    Add string example

--- schema_extender_test/extender.py
+++ schema_extender_test/extender.py
@@ -3,6 +3,8 @@ from Products.Archetypes.public import BooleanField
 from Products.Archetypes.public import BooleanWidget
 from Products.Archetypes.public import LinesField
 from Products.Archetypes.public import MultiSelectionWidget
+from Products.Archetypes.public import StringField
+from Products.Archetypes.public import StringWidget
 from archetypes.schemaextender.field import ExtensionField
 from archetypes.schemaextender.interfaces import ISchemaExtender
 from zope.component import adapts
@@ -13,6 +15,10 @@ class MyBooleanField(ExtensionField, BooleanField):
     """A trivial field."""
 
 
+class MyStringField(ExtensionField, StringField):
+    """Another trivial field."""
+
+
 class PageExtender(object):
     adapts(IATDocument)
     implements(ISchemaExtender)
@@ -21,6 +27,12 @@ class PageExtender(object):
         MyBooleanField("super_power",
         widget=BooleanWidget(
             label="This page has super powers")),
+            ]
+
+    fields = [
+        MyStringField("super_power2",
+        widget=StringWidget(
+            label="This page has super powers too")),
             ]
 
     def __init__(self, context):
