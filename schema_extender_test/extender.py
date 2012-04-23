9ea27ae37c049459ca1035cd16709a60cb3e1061
Alex Clark <aclark@aclark.net>
2012-04-23 10:22:56 -0400

    WTF

--- schema_extender_test/extender.py
+++ schema_extender_test/extender.py
@@ -9,6 +9,11 @@ from archetypes.schemaextender.field import ExtensionField
 from archetypes.schemaextender.interfaces import ISchemaExtender
 from zope.component import adapts
 from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+def MyVocabFactory():
+   return SimpleVocabulary.fromValues(['one', 'two', 'three'])
 
 
 class MyBooleanField(ExtensionField, BooleanField):
@@ -19,18 +24,35 @@ class MyStringField(ExtensionField, StringField):
     """Another trivial field."""
 
 
+class MyLinesField(ExtensionField, LinesField):
+    """A non-trivial field."""
+
+
 class PageExtender(object):
     adapts(IATDocument)
     implements(ISchemaExtender)
 
     fields = [
+
         MyBooleanField("super_power",
-        widget=BooleanWidget(
-            label="This page has super powers")),
+            widget=BooleanWidget(
+                label="This page has super powers"
+            )
+        ),
+
         MyStringField("super_power2",
-        widget=StringWidget(
-            label="This page has super powers too")),
-            ]
+            widget=StringWidget(
+                label="This page has super powers too"
+            )
+        ),
+
+        MyLinesField("super_power3",
+            vocabulary=MyVocabFactory(),
+            widget=MultiSelectionWidget(
+                label="This page has super powers three"
+            )
+        ),
+    ]
 
     def __init__(self, context):
         self.context = context
